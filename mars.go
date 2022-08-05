463e86448346708ee4b1656899f55c78c8695dbe
qz <1875971807@qq.com>
2022-08-05 10:12:17 +0800

    go 语言中的 算数基本操作

---
+++ mars.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("My weight on hte surface of Mars is ")
+	fmt.Print(149.0 * 0.3783)
+	fmt.Print("lbs, and I would be  ")
+	fmt.Print(41 + 365/687)
+	fmt.Print("years old.")
+
+	fmt.Println("My weight on the surface of Mars is ", 179*0.3783)
+
+}
